50ebd4bafcd5e7bfdfe6d3fdfe6d3c4804a25fd5
yoshi <yoshiaki11205656@gmail.com>
2020-12-23 23:47:11 +0900

    引数の型

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('ここにコードを追加します!');
+function add(num1, num2) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        throw new Error('入力値が数値ではありません');
+    }
+    return num1 + num2;
+}
+var num1 = '5';
+var num2 = 10;
+var result = add(num1, num2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log('ここにコードを追加します!');
+function add(num1: number, num2: number) {
+    if ( typeof num1 !== 'number' || typeof num2 !== 'number' ){
+        throw new Error('入力値が数値ではありません')
+    }
+    return num1 + num2
+}
+
+const num1 = 5
+// const num1 = '5'
+const num2 = 10
+
+const result = add(num1, num2)
+
+console.log(result);
