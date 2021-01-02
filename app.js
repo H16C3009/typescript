67c4e60af1de07e7014aac8eb2d3680fa9e1247a
yoshi <yoshiaki11205656@gmail.com>
2021-01-02 14:43:01 +0900

    object

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
-function add(n1, n2, showResult, phrase) {
-    // if ( typeof n1 !== 'number' || typeof 2 !== 'number' ){
-    //     throw new Error('入力値が数値ではありません')
-    // }
-    if (showResult) {
-        console.log(phrase + (n1 + n2));
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var num1 = 5;
-// const num1 = '5'
-var num2 = 10;
-var printResult = true;
-var resultPhrase = 'Result: ';
-var result = add(num1, num2, printResult, resultPhrase);
-// console.log(result);
+// const person: {
+//     name: string;
+//     age: number;
+// } 
+var person = {
+    name: 'yota',
+    age: 31
+};
+console.log(person);
--- app.ts
+++ app.ts
@@ -1,20 +1,11 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    // if ( typeof n1 !== 'number' || typeof 2 !== 'number' ){
-    //     throw new Error('入力値が数値ではありません')
-    // }
-    if (showResult) {
-        console.log(phrase + (n1 + n2))
-    } else {
-        return n1 + n2
-    }
-}
+// const person: {
+//     name: string;
+//     age: number;
+// } 
+const person = {
+    name: 'yota',
+    age: 31,
+};
 
-const num1 = 5
-// const num1 = '5'
-const num2 = 10
-const printResult = true
-const resultPhrase = 'Result: '
+console.log(person)
 
-const result = add(num1, num2, printResult, resultPhrase)
-
-// console.log(result);
