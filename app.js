d8a51a9cdaec9c9e06e69eaa398a879f99b23a40
yoshi <yoshiaki11205656@gmail.com>
2021-01-02 22:54:00 +0900

    function-callback

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result: ' + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+    return result;
+});
 // let combineValues: Function;
 var combineValues;
 combineValues = add;
 // combineValues = printResult;
 // combineValues = 5;
-console.log(combineValues(8, 8));
-console.log(printResult(add(5, 12)));
+// console.log(combineValues(8, 8));
+// console.log(printResult(add(5, 12)))
--- app.ts
+++ app.ts
@@ -7,6 +7,17 @@ function printResult(num: number): void {
     console.log('Result: ' + num)
 }
 
+function addAndHandle(n1: number,n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result)
+}
+
+addAndHandle(10, 20, (result) => {
+    console.log(result);
+    // return result;
+});
+
+
 // let combineValues: Function;
 let combineValues: (a:number, b:number) => number;
 
@@ -14,5 +25,5 @@ combineValues = add;
 // combineValues = printResult;
 // combineValues = 5;
 
-console.log(combineValues(8, 8));
-console.log(printResult(add(5, 12)))
+// console.log(combineValues(8, 8));
+// console.log(printResult(add(5, 12)))
