fb5e06cf17310ce7d177d68f7e4ac5bf206ba75c
yoshi <yoshiaki11205656@gmail.com>
2021-01-02 14:57:41 +0900

    tuple

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var person = {
     name: 'yota',
     age: 31,
-    hobbies: ['sports', 'cooking']
+    hobbies: ['sports', 'cooking'],
+    role: [2, 'author']
 };
+// person.role.push('admin')
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user']
 var favoriteActivities;
-favoriteActivities = ['sports'];
+favoriteActivities = ['sports', 31];
 console.log(person);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
--- app.ts
+++ app.ts
@@ -1,8 +1,19 @@
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
     name: 'yota',
     age: 31,
     hobbies: ['sports', 'cooking'],
+    role: [2, 'author'],
 };
+
+// person.role.push('admin')
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user']
 
 let favoriteActivities: any[];
 favoriteActivities = ['sports', 31];
