cee3eaa3d3af13e7ee2922a829593ade4a3e26c0
yoshi <yoshiaki11205656@gmail.com>
2021-01-02 23:57:26 +0900

    never

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ userInput = 'Max';
 if (typeof userInput === 'string') {
     userName = userInput;
 }
+function generateError(message, code) {
+    // スクリプトがクラッシュするかtry-catchするか（絶対に値を返さない）
+    throw { message: message, errorCode: code };
+    // while (true);
+}
+var result = generateError('happend server error', 500);
+console.log(result);
--- app.ts
+++ app.ts
@@ -8,3 +8,12 @@ if (typeof userInput === 'string') {
     userName = userInput
 }
 
+function generateError(message: string, code: number): never {
+    // スクリプトがクラッシュするかtry-catchするか（絶対に値を返さないnever型）
+    throw { message: message, errorCode: code }
+    // while (true);
+}
+
+const result = generateError('happend server error', 500);
+console.log(result)
+
