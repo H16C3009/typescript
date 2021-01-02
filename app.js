20f7b2db141d449dea0e4b17db89b096fb127ef2
yoshi <yoshiaki11205656@gmail.com>
2021-01-02 21:54:58 +0900

    literal

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 // 引数に対して、複数の型を指定することができる（タプル型）
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' ||
+        resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
+    // if (resultConversion === 'as-number') {
+    //     return +result
+    //     // return parseFloat(result)
+    // } else {
+    //     return result.toString()
+    // }
     // const result = input1 + input2
     return result;
 }
-console.log(combine(30, 26));
-console.log(combine('Max', 'Anna'));
+console.log(combine(30, 26, 'as-number'));
+console.log(combine('30', '26', 'as-number'));
+console.log(combine('Max', 'Anna', 'as-text'));
--- app.ts
+++ app.ts
@@ -1,15 +1,28 @@
 // 引数に対して、複数の型を指定することができる（タプル型）
-function combine(input1: number | string, input2: number | string) {
+function combine(
+    input1: number | string,
+    input2: number | string,
+    resultConversion: 'as-number' | 'as-text',
+) {
     let result;
-    if ( typeof input1 === 'number' && typeof input2 === 'number' ) {
-        result = input1 + input2
+    if (typeof input1 === 'number' && typeof input2 === 'number' ||
+        resultConversion === 'as-number') {
+        result = +input1 + +input2
     } else {
         result = input1.toString() + input2.toString()
     }
+    // if (resultConversion === 'as-number') {
+    //     return +result
+    //     // return parseFloat(result)
+    // } else {
+    //     return result.toString()
+    // }
     // const result = input1 + input2
     return result
 }
 
-console.log(combine(30, 26))
+console.log(combine(30, 26, 'as-number'))
 
-console.log(combine('Max', 'Anna'))
+console.log(combine('30', '26', 'as-number'))
+
+console.log(combine('Max', 'Anna', 'as-text'))
