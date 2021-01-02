b47c680b3989eb6ef7a47d67e06c6172eb6cf110
yoshi <yoshiaki11205656@gmail.com>
2021-01-02 14:50:08 +0900

    array

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-// const person: {
-//     name: string;
-//     age: number;
-// } 
 var person = {
     name: 'yota',
-    age: 31
+    age: 31,
+    hobbies: ['sports', 'cooking']
 };
+var favoriteActivities;
+favoriteActivities = ['sports'];
 console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -1,11 +1,14 @@
-// const person: {
-//     name: string;
-//     age: number;
-// } 
 const person = {
     name: 'yota',
     age: 31,
+    hobbies: ['sports', 'cooking'],
 };
+
+let favoriteActivities: any[];
+favoriteActivities = ['sports', 31];
 
 console.log(person)
 
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase())
+}
