764a9a28915b2c1d7c4abe820c02bfcfcf7a47a9
yoshi <yoshiaki11205656@gmail.com>
2021-01-03 16:33:19 +0900

    public-private-modifier

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,24 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
     }
     describe() {
         console.log('department:' + this.name);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInfomation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 const accounting = new Department('Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
 console.log(accounting);
 accounting.describe();
-const accountingCopy = { name: 'dummy', describe: accounting.describe };
-accountingCopy.describe();
+accounting.printEmployeeInfomation();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 class Department {
-    name: string
+    // default is public
+    private name: string;
+    private employees: string[] = []
 
     constructor(n: string) {
         this.name = n
@@ -8,12 +10,28 @@ class Department {
     describe(this: Department) {
         console.log('department:' + this.name)
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee)
+    }
+
+    printEmployeeInfomation() {
+        console.log(this.employees.length)
+        console.log(this.employees)
+    }
 }
 
 const accounting = new Department('Accounting')
+accounting.addEmployee('Max')
+accounting.addEmployee('Manu')
+
+// accounting.employees[2] = 'Anna'
+// accounting.name = 'New Name'
+
 console.log(accounting)
 accounting.describe()
+accounting.printEmployeeInfomation()
 
-const accountingCopy = { name: 'dummy', describe: accounting.describe }
-accountingCopy.describe()
+// const accountingCopy = { name: 'dummy', describe: accounting.describe }
+// accountingCopy.describe()
 
