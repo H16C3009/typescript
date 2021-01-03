cd84e1e60e25ba4f6b2bad4f37d0157de2ec7a98
yoshi <yoshiaki11205656@gmail.com>
2021-01-03 16:13:33 +0900

    constructor-this-keyword

--- dist/app.js
+++ dist/app.js
@@ -3,7 +3,13 @@ class Department {
     constructor(n) {
         this.name = n;
     }
+    describe() {
+        console.log('department:' + this.name);
+    }
 }
 const accounting = new Department('Accounting');
 console.log(accounting);
+accounting.describe();
+const accountingCopy = { name: 'dummy', describe: accounting.describe };
+accountingCopy.describe();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,16 @@ class Department {
     constructor(n: string) {
         this.name = n
     }
+
+    describe(this: Department) {
+        console.log('department:' + this.name)
+    }
 }
 
 const accounting = new Department('Accounting')
 console.log(accounting)
+accounting.describe()
 
-
+const accountingCopy = { name: 'dummy', describe: accounting.describe }
+accountingCopy.describe()
 
