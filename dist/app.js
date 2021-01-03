003c57837c3321ff093c33fccc0619abca77ae67
yoshi <yoshiaki11205656@gmail.com>
2021-01-03 20:22:47 +0900

    first-interface

--- dist/app.js
+++ dist/app.js
@@ -1,92 +1,11 @@
 "use strict";
-class Department {
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
-        this.employees = [];
-        console.log(this.name);
-    }
-    static createEmployee(name) {
-        return { name: name };
-    }
-    addEmployee(employee) {
-        this.employees.push(employee);
-    }
-    printEmployeeInfomation() {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    }
-}
-Department.fiscalYear = 2020;
-class ITDepartment extends Department {
-    constructor(id, admins) {
-        super(id, 'IT');
-        this.admins = admins;
-    }
-    describe() {
-        console.log('IT部門・ID：' + this.id);
-    }
-}
-class AccountingDepartment extends Department {
-    constructor(id, reports) {
-        super(id, 'Accounting');
-        this.reports = reports;
-        this.lastReport = reports[0];
-    }
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport;
-        }
-        throw new Error('レポートがありません。');
-    }
-    set mostRecentReport(value) {
-        if (!value) {
-            throw new Error('正しい値を設定して下さい。');
-        }
-        this.addReport(value);
-    }
-    static getInstance() {
-        if (AccountingDepartment.instance) {
-            return this.instance;
-        }
-        this.instance = new AccountingDepartment('D2', []);
-        return this.instance;
-    }
-    describe() {
-        console.log('会計部門・ID：' + this.id);
-    }
-    addReport(text) {
-        this.reports.push(text);
-        this.lastReport = text;
-    }
-    printReports() {
-        console.log(this.reports);
-        console.log(this.lastReport);
-    }
-    addEmployee(name) {
-        if (name === 'Max') {
-            return;
-        }
-        this.employees.push(name);
-    }
-}
-const employee1 = Department.createEmployee('Max');
-console.log(employee1, Department.fiscalYear);
-const it = new ITDepartment('D1', ['Max']);
-it.addEmployee('Max');
-it.addEmployee('Manu');
-console.log(it);
-it.describe();
-it.printEmployeeInfomation();
-const accounting = AccountingDepartment.getInstance();
-const accounting2 = AccountingDepartment.getInstance();
-console.log(accounting, accounting2);
-accounting.mostRecentReport = 'settter something';
-accounting.addReport('Something1');
-accounting.addReport('Something2');
-console.log(accounting.mostRecentReport);
-accounting.addEmployee('Max');
-accounting.addEmployee('Manu');
-accounting.describe();
-console.log(accounting);
+let user1;
+user1 = {
+    name: 'Max',
+    age: 30,
+    greeting(phrase) {
+        console.log(phrase + ' ' + this.name);
+    },
+};
+user1.greeting('I am');
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,161 +1,21 @@
-// abstractクラスはインスタンス作成できない。継承されるためのクラスである。
-abstract class Department {
-    // default is public
-    // private name: string;
-    // private:外部、外部クラスからアクセスできない
-    // private employees: string[] = []
-    // protected:外部からアクセスできないが、このクラスを継承したサブ（派生）クラスからはアクセスできる
-    protected employees: string[] = []
-    // static property
-    static fiscalYear = 2020
+interface Person {
+    name: string;
+    age: number;
 
-    // static method
-    static createEmployee(name: string) {
-        return { name: name }
-    }
-
-    constructor(protected readonly id: string, private name: string) {
-        // this.name = n
-        // console.log(Department.fiscalYear)
-        console.log(this.name)
-    }
-
-    abstract describe(this: Department): void;
-
-    addEmployee(employee: string) {
-        // this.id = 'D2'
-        this.employees.push(employee)
-    }
-
-    printEmployeeInfomation() {
-        console.log(this.employees.length)
-        console.log(this.employees)
-    }
-}
-
-// 継承元クラス＝ベースクラス
-// 継承先（派生）クラス＝サブクラス
-class ITDepartment extends Department {
-    admins: string[];
-
-    constructor(id: string, admins: string[]) {
-        super(id, 'IT')
-        this.admins = admins
-    }
-
-    describe() {
-        console.log('IT部門・ID：' + this.id)
-    }
-}
-
-// class DevelopmentSection extends ITDepartment {
-//     addEmployee(name: string) {
-//         if (name === 'Max') {
-//             return;
-//         }
-//         this.employees.push(name)
-//     }
-// }
-
-
-class AccountingDepartment extends Department {
-    private lastReport: string;
-    private static instance: AccountingDepartment;
-
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport
-        }
-        throw new Error('レポートがありません。')
-    }
-
-    set mostRecentReport(value: string) {
-        if (!value) {
-            throw new Error('正しい値を設定して下さい。')
-        }
-        this.addReport(value)
-    }
-
-    private constructor(id: string, private reports: string[]) {
-        super(id, 'Accounting')
-        this.lastReport = reports[0]
-    }
-
-    static getInstance() {
-        if (AccountingDepartment.instance) {
-            return this.instance
-        }
-        this.instance = new AccountingDepartment('D2', [])
-        return this.instance;
-    }
-
-    describe() {
-        console.log('会計部門・ID：' + this.id)
-    }
-
-    addReport(text: string) {
-        this.reports.push(text)
-        this.lastReport = text
-    }
-
-    printReports() {
-        console.log(this.reports)
-        console.log(this.lastReport)
-    }
-
-    addEmployee(name: string) {
-        if (name === 'Max') {
-            return;
-        }
-        this.employees.push(name)
-    }
+    greeting(phrase: string): void;
 }
 
 
-const employee1 = Department.createEmployee('Max')
-console.log(employee1, Department.fiscalYear)
+let user1: Person;
 
-const it = new ITDepartment('D1', ['Max'])
-it.addEmployee('Max')
-it.addEmployee('Manu')
+user1 = {
+    name: 'Max',
+    age: 30,
 
-console.log(it)
-it.describe()
-it.printEmployeeInfomation()
-
-
-// const accounting = new AccountingDepartment('D2', [])
-const accounting = AccountingDepartment.getInstance();
-const accounting2 = AccountingDepartment.getInstance();
-console.log(accounting, accounting2)
-
-// Error setter & getter
-// console.log(accounting.mostRecentReport)
-// accounting.mostRecentReport = ''
-
-accounting.mostRecentReport = 'settter something'
-
-accounting.addReport('Something1')
-accounting.addReport('Something2')
-console.log(accounting.mostRecentReport)
-// override method
-accounting.addEmployee('Max')
-accounting.addEmployee('Manu')
-
-accounting.describe()
-
-// accounting.printReports()
-// accounting.printEmployeeInfomation()
-console.log(accounting)
+    greeting(phrase: string) {
+        console.log(phrase + ' ' + this.name)
+    },
+}
 
 
-// const a = new DevelopmentSection('D3', [])
-
-// // override method
-// a.addEmployee('Maxa')
-// a.addEmployee('Manua')
-
-// // a.employees[1] = 'max'
-
-// a.printEmployeeInfomation()
-// console.log(a)
+user1.greeting('I am')
